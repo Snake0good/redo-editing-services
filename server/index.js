b0764a77c6599eb4cfcce5d27a2461bdfe2cf567
Jacob Good <jacobegood@gmail.com>
2022-09-13 22:23:56 -0400

    added packages

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/database');
 const app = express();
+const PORT = process.env.PORT
 
 // use .env file in config folder
 require("dotenv").config({ path: "./config/.env" })
 
-const PORT = process.env.PORT
+
+// connect to the database
+connectDB()
+
+
 
 // server running
 app.listen(PORT, () => {
