356dd1f1a1964ee887c01b5b3517031bdea20e1f
Jacob Good <jacobegood@gmail.com>
2022-09-13 22:16:12 -0400

    a bit of css

---
+++ README.md
@@ -0,0 +1,5 @@
+# Editing Services by John
+This is for my client, we'll call him John, that needed a website to signup, login, upload documents, download documents, and pay via Stripe. 
+
+# Things I Learned
+A lot. I mean, this was the first time I really dove into a full-stack project and created it from scratch. 
--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 
+// use .env file in config folder
+require("dotenv").config({ path: "./config/.env" })
+
+const PORT = process.env.PORT
+
 // server running
-app.listen(process.env.PORT, () => {
-    console.log("Server is running, you better get it!")
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}, you better get it!`)
 })
